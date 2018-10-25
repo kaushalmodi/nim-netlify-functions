a99f70252a706851d76ece010043db68a13acfc2
Kaushal Modi <kaushal.modi@gmail.com>
2018-10-25 18:18:15 -0400

    Add the earlier non-working hello_name.nim as hello_name_bad.nim

---
+++ src/lambda/hello_name_bad.nim
@@ -0,0 +1,19 @@
+import jsffi
+import asyncjs
+
+var exports {.importc.}: js
+#           ^^^^^^^^^^^ -- This *has* to be between var name and the "js" type.
+
+exports.handler = proc(event: js, context: js): Future[js] {.async.} =
+  var
+    name = event.queryStringParameters.name.to(string)
+  if name == "":
+    name = "World"
+
+  return js{
+    statusCode: 200,
+    body: cstring("Hello, " & name & "! From Nim.")
+    }
+
+# https://nim-lang.org/docs/asyncjs.html
+# https://gitter.im/nim-lang/Nim?at=5bd22ab6435c2a518e0518c1
