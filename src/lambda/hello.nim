3c13396d517097750f5acb39c3d60d498addd0bf
Kaushal Modi <kaushal.modi@gmail.com>
2018-10-25 16:42:33 -0400

    Add Nim source for hello.js

---
+++ src/lambda/hello.nim
@@ -0,0 +1,9 @@
+import jsffi
+
+var exports {.importc.}: js
+#           ^^^^^^^^^^^ -- This *has* to be between var name and the "js" type.
+exports.handler = proc(event: js, context: js, callback: proc(n: js, obj: js)) =
+  callback(nil, js{
+    statusCode: 200,
+    body: cstring"Hello, World"}
+  )
