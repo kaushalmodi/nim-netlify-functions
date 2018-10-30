1cb27bae9fedf94d10ddbed8a5a865b6e3fff7e4
Kaushal Modi <kaushal.modi@gmail.com>
2018-10-29 22:17:37 -0400

    Another attempt

--- src/lambda/nimcache/hello_name_post_orig.js
+++ src/lambda/nimcache/hello_name_post_orig.js
@@ -8,7 +8,7 @@ exports.handler = async (event, context) => {
 
   // When the method is POST, the name will no longer be in the event’s
   // queryStringParameters – it’ll be in the event body encoded as a query string
-  const params = qs.querystring.parse(event.body);
+  const params = querystring.parse(event.body);
   const name = params.name || "World";
 
   return {
