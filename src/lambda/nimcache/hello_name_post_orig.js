1c4f71d724d6383fd60d1b1c64faf5f8180ad8af
Kaushal Modi <kaushal.modi@gmail.com>
2018-10-29 22:14:54 -0400

    See if using require helps
    https://stackoverflow.com/questions/39436322/node-js-syntaxerror-unexpected-token-import

--- src/lambda/nimcache/hello_name_post_orig.js
+++ src/lambda/nimcache/hello_name_post_orig.js
@@ -1,4 +1,4 @@
-import querystring from "querystring";
+const qs = require("querystring");
 
 exports.handler = async (event, context) => {
   // Only allow POST
@@ -8,7 +8,7 @@ exports.handler = async (event, context) => {
 
   // When the method is POST, the name will no longer be in the event’s
   // queryStringParameters – it’ll be in the event body encoded as a query string
-  const params = querystring.parse(event.body);
+  const params = qs.querystring.parse(event.body);
   const name = params.name || "World";
 
   return {
